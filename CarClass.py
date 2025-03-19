df9d292d967e9a05e73052324ad6d99f66685bee
Dmitriy <dimasikemelka@gmail.com>
2025-03-19 10:54:57 +0200

    added classs Car

---
+++ CarClass.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, id, name,  typeOfEngine, volumeOfEngine, typeOfFuel, CreationYear, Country, actionBet):
+        self.id = id
+        self.name = name
+        self.typeOfEngine = typeOfEngine
+        self.volumeOfEngine = volumeOfEngine # ob`em / kw
+        self.typeOfFuel = typeOfFuel
+        self.CreationYear = CreationYear
+        self.Country = Country # по желанию удалить
+        self.actionBet = actionBet # ayk stavka
